925e9d23934a33664ac8008973c600fc57937d25
Anna <goryachevaanny@gmail.com>
2021-06-14 19:24:50 +0300

    Initial Commit

---
+++ PoliceBase/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PoliceBase
+//
+//  Created by Анна Горячева on 14.06.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
